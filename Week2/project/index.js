475a724b30d48ba89c483cc55cab53584f4db41c
Ahsen M <aahsenmedar@gmail.com>
2021-01-11 00:55:52 +0100

    Added project

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -8,3 +8,31 @@
   If the timer finishes the timer should be replaced by the message: Time 's up!
  * 
  */
+
+"use strict";
+const increment = document.getElementById("increment");
+let lengthOfSession = document.getElementById("lengthOfSession")
+const decrement = document.getElementById("document");
+let minute = document.getElementById("minute");
+let semicolon = document.getElementById("semicolon");
+let second = document.getElementById("second");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+
+let timer;
+lengthOfSession.innerText = minute.innerText;
+
+
+// I found these solutions to add or decrease a number.
+const increase = () => {
+  minute.innerText++;
+  lengthOfSession.innerHTML = minute.innerText;
+};
+
+const decrease = () => {
+  minute.innerHTML--;
+  lengthOfSession.innerText = minute.innerText;
+}
+
+//I couldn't find a solution for pause and stop. Also when play is clicked the arrows work too. 
+// I couldn't find a solution for prevent to change the length of session. Sorry, but I get confused to finish this project.
